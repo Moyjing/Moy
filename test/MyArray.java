1aa1e258afd09b295d0a6008f37239526f145904
Moyjing <55129181+Moyjing@users.noreply.github.com>
2019-09-25 20:39:58 +0800

    Update MyArray.java

--- test/MyArray.java
+++ test/MyArray.java
@@ -1,1 +1,19 @@
 
+public class MyArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int m[][];
+		m=new int [4][4];
+		m[0][0]=1;
+		m[1][1]=1;
+		m[2][2]=1;
+		m[3][3]=1;
+		System.out.println(m[0][0]+""+m[0][1]+""+m[0][2]+""+m[0][3]);
+		System.out.println(m[1][0]+""+m[1][1]+""+m[1][2]+""+m[1][3]);
+		System.out.println(m[2][0]+""+m[2][1]+""+m[2][2]+""+m[2][3]);
+		System.out.println(m[3][0]+""+m[3][1]+""+m[3][2]+""+m[3][3]);
+		
+	}
+
+}

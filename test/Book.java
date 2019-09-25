9c916ee33613bfac22f3e591b78ad1afea814d27
Moyjing <55129181+Moyjing@users.noreply.github.com>
2019-09-25 20:39:07 +0800

    Update Book.java

--- test/Book.java
+++ test/Book.java
@@ -1,1 +1,27 @@
+package 第12题;
 
+public class Book {
+	String  Bookname;
+	String Author;
+	int Monthlysalas;
+	Book(){
+	}
+	Book(String Bookname,String Author,int Monthlysalas){
+	   this.Bookname=Bookname;
+	   this.Author=Author;
+	   this.Monthlysalas=Monthlysalas;
+	}
+	void setBook(String Bookname,String Author,int Monthlysalas){
+	   this.Bookname=Bookname;
+	   this.Author=Author;
+	   this.Monthlysalas=Monthlysalas;
+	}
+	void printBook(){
+	   Book Book1=new Book();
+	   Book1.setBook("To live","Yu Hua",1000);
+	System.out.println("第一次属性设置："+"书名："+Book1.Bookname+','+"作者："+Book1.Author+','+"月销售量："+Book1.Monthlysalas);
+	Book Book2=new Book("The Old Man and Sea","Hemingway",1500);
+	System.out.println("第二次属性设置："+"书名："+Book2.Bookname+','+"作者："+Book2.Author+','+"月销售量："+Book2.Monthlysalas);
+	}
+	
+}
